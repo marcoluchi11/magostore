5fe63b42ae4ffbee064c3640443de94bbd460c4c
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-09 11:29:04 -0300

    reactrouterdom scaffolding added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Products from "./components/Products";
+import Contact from "./components/Contact";
+import Whoarewe from "./components/Whoarewe";
 function App() {
   return (
-    <div>
-      <h1>MAGO STORE</h1>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/products" component={Products}></Route>
+          <Route exact path="/contact" component={Contact}></Route>
+          <Route exact path="/whoarewe" component={Whoarewe}></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
