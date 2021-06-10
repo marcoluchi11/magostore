c73f7003dff29c6d85575f5c5a2ff6308ee14490
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-10 09:41:28 -0300

    added footer at bottom

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <footer>
+      <a
+        href="https://cafecito.app/marcoluchi11"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <img
+          srcset="https://cdn.cafecito.app/imgs/buttons/button_3.png 1x, https://cdn.cafecito.app/imgs/buttons/button_3_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_3_3.75x.png 3.75x"
+          src="https://cdn.cafecito.app/imgs/buttons/button_3.png"
+          alt="Invitame un café en cafecito.app"
+        />
+      </a>
+    </footer>
+  );
+};
+
+export default Footer;
