68522f4ea0957445cb04e06939e0e064bad3bb59
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-10 09:41:49 -0300

    added footer at bottom

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Products from "./components/Products";
 import Contact from "./components/Contact";
 import Whoarewe from "./components/Whoarewe";
+import Footer from "./components/Footer";
 function App() {
   return (
     <Router>
-      <div>
+      <main>
         <Navbar />
         <Switch>
           <Route exact path="/products" component={Products}></Route>
           <Route exact path="/contact" component={Contact}></Route>
           <Route exact path="/whoarewe" component={Whoarewe}></Route>
         </Switch>
-      </div>
+      </main>
+      <Footer />
     </Router>
   );
 }
