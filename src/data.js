fdc36d1a1f100b4d62c679e0de801a77a1c79ee6
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-13 11:06:38 -0300

    added local data for pruebas

---
+++ src/data.js
@@ -0,0 +1,57 @@
+export const products = [
+  {
+    id: 1,
+    image:
+      "https://images.footlocker.com/is/image/EBFL2/42388153_a1?wid=519&hei=519&fmt=png-alpha",
+    description: "Camiseta",
+    brand: "Nike",
+    price: "$7000",
+  },
+  {
+    id: 2,
+    image: "https://i.ebayimg.com/images/g/dlQAAOSw161exrjo/s-l300.jpg",
+    brand: "Nike",
+    description: "Zapatillas Kobe 11",
+    price: "$3500",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.footlocker.com/is/image/EBFL2/42006001_a1?wid=519&hei=519&fmt=png-alpha",
+    brand: "Adidas",
+    description: "Camiseta",
+    price: "$4550",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.footlocker.com/is/image/EBFL2/EK1749_a1?wid=519&hei=519&fmt=png-alpha",
+    brand: "Reebok",
+    price: "$1250",
+    description: "Camiseta",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.footlocker.com/is/image/EBFL2/EK1749_a1?wid=519&hei=519&fmt=png-alpha",
+    brand: "Reebok",
+    price: "$5000",
+    description: "Camiseta",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.footlocker.com/is/image/EBFL2/EK1749_a1?wid=519&hei=519&fmt=png-alpha",
+    brand: "Reebok",
+    price: "$1945",
+    description: "Camiseta",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.footlocker.com/is/image/EBFL2/EK1749_a1?wid=519&hei=519&fmt=png-alpha",
+    brand: "Reebok",
+    price: "$12000",
+    description: "Camiseta",
+  },
+];
