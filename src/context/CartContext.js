a385b31a0e2c6db157cc0576f92d2e534ab3d7c3
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-15 10:13:34 -0300

    added total as state for  calculate finalprice

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -3,8 +3,9 @@ export const CartContext = createContext();
 
 const CartProvider = (props) => {
   const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider value={{ cart, total, setCart, setTotal }}>
       {props.children}
     </CartContext.Provider>
   );
