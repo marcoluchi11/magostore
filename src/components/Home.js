816e525be391bb2b4817029c8b6a7d3bfe4cd609
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-15 10:13:10 -0300

    added spinner for trying

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import Spinner from "./Spinner";
 
 const Home = () => {
   return (
     <Fragment>
       <h4>HOME</h4>
+      <Spinner />
     </Fragment>
   );
 };
