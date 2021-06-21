6af82cb331cffd67722ae1ec58f54b3bfa1016a8
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-21 16:15:45 -0300

    added pushed products to firebase db

--- src/components/FormAdmin.js
+++ src/components/FormAdmin.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { useState, useEffect } from "react";
+import fire from "./../firebaseConfig";
 const Formulario = styled.form`
   display: flex;
   flex-direction: column;
@@ -33,18 +35,103 @@ const Formulario = styled.form`
     font-size: 1.2rem;
   }
 `;
+
 const FormAdmin = () => {
+  const [url, setUrl] = useState("");
+  const [image, setImage] = useState(null);
+  const [data, setData] = useState({
+    title: "",
+    description: "",
+    price: 0,
+  });
+  const [upload, setUpload] = useState(false);
+
+  useEffect(() => {
+    if (upload) {
+      fire
+        .storage()
+        .ref("images")
+        .child(image.name)
+        .getDownloadURL()
+        .then((url) => {
+          setUrl(url);
+        });
+    }
+  }, [upload, image]);
+  const handleUpload = (e) => {
+    e.preventDefault();
+    fire
+      .storage()
+      .ref(`images/${image.name}`)
+      .put(image)
+      .then((snapshot) => {
+        console.log("file uploaded!");
+        setUpload(true);
+      });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const db = fire.firestore();
+    db.collection("productos")
+      .add({ ...data, imagen: url })
+      .then(() => {
+        console.log("Value successfully written!");
+        setData({ title: "", description: "", price: 0 });
+        setImage(null);
+        setUrl("");
+        setUpload(false);
+      })
+      .catch((error) => {
+        console.log("Uups! we've had an error", error);
+      });
+  };
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleImage = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    }
+  };
   return (
-    <Formulario>
-      <label name="title">Titulo</label>
-      <input name="title" type="text" />
+    <Formulario onSubmit={handleSubmit}>
+      <label htmlFor="title" name="title">
+        Titulo
+      </label>
+      <input
+        id="title"
+        autoComplete="off"
+        onChange={handleChange}
+        name="title"
+        type="text"
+        value={data.title}
+      />
       <label name="description">Descripcion</label>
-      <input name="description" type="text" />
+      <input
+        autoComplete="off"
+        onChange={handleChange}
+        name="description"
+        type="text"
+        value={data.description}
+      />
       <label name="price">Precio</label>
-      <input name="price" type="number" />
-      <label>Imagen</label>
-      <input type="file" />
-      <input type="submit" value="Agregar" />
+      <input
+        autoComplete="off"
+        onChange={handleChange}
+        name="price"
+        type="number"
+        value={data.price}
+      />
+      <label htmlFor="image" name="image">
+        Imagen
+      </label>
+
+      <input onChange={handleImage} type="file" name="image" id="image" />
+      {url}
+      <button onClick={handleUpload}>Upload</button>
+      <input type="submit" value="Agregar producto" />
     </Formulario>
   );
 };
