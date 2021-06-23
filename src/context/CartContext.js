f9f696cfa3c86ef8a0031a8e28e7b4553a1796d3
marcoluchi11 <marcoaluchi@hotmail.com>
2021-06-23 16:40:54 -0300

    added products state for global use

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -4,8 +4,9 @@ export const CartContext = createContext();
 const CartProvider = (props) => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
+  const [products, setProducts] = useState([]);
   return (
-    <CartContext.Provider value={{ cart, total, setCart, setTotal }}>
+    <CartContext.Provider value={{ cart, products,total, setCart, setProducts, setTotal }}>
       {props.children}
     </CartContext.Provider>
   );
