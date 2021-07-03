d280ab2f6d4344dd2631423a5558a014820ba4a7
marcoluchi11 <marcoaluchi@hotmail.com>
2021-07-03 11:12:04 -0300

    added state for addtocart warning

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -5,8 +5,20 @@ const CartProvider = (props) => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
   const [products, setProducts] = useState([]);
+  const [added, setAdded] = useState(false);
   return (
-    <CartContext.Provider value={{ cart, products,total, setCart, setProducts, setTotal }}>
+    <CartContext.Provider
+      value={{
+        cart,
+        added,
+        products,
+        total,
+        setAdded,
+        setCart,
+        setProducts,
+        setTotal,
+      }}
+    >
       {props.children}
     </CartContext.Provider>
   );
