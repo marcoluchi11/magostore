375eacac8676c32a3577bc66c15c07dd022f0d5c
marcoluchi11 <marcoaluchi@hotmail.com>
2021-07-12 16:57:26 -0300

    delete usestate from dependencies

--- src/components/Admin/Welcome.js
+++ src/components/Admin/Welcome.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import AddProduct from "./AddProduct";
 import DeleteProduct from "./DeleteProduct";
