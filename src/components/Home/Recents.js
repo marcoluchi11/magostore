66ef86944147d94b33a0ee3c62d98d697205ad22
marcoluchi11 <marcoaluchi@hotmail.com>
2021-07-16 18:57:39 -0300

    add carousel in ingresos recientes

--- src/components/Home/Recents.js
+++ src/components/Home/Recents.js
@@ -1,8 +1,86 @@
+import { nanoid } from "nanoid";
+import React, { useEffect, useContext } from "react";
+import Carousel from "react-elastic-carousel";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { CartContext } from "../../context/CartContext";
+import fire from "../../firebaseConfig";
+import Spinner from "./../Spinner";
+const Seccion = styled.section`
+  min-height: 90vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  h1 {
+    font-size: 2.6rem;
+    font-weight: 400;
+    margin: 1rem;
+  }
+  div {
+    img {
+      width: 15rem;
+      height: 15rem;
+    }
+  }
+`;
+
 const Recents = () => {
+  const { setProducts, products } = useContext(CartContext);
+  const getProducts = async () => {
+    if (products.length === 0) {
+      let productos = [];
+      const db = fire.firestore();
+      await db
+        .collection("productos")
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            const newProduct = {
+              imagen: doc.data().imagen,
+              title: doc.data().title,
+              description: doc.data().description,
+              price: Number(doc.data().price),
+              id: doc.data().id,
+              size: doc.data().size,
+              error: false,
+            };
+            productos.push(newProduct);
+          });
+        });
+      setProducts(productos);
+    }
+  };
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const bpointovich = [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
+  ];
+
   return (
-    <section>
+    <Seccion>
       <h1>Ingresos más recientes</h1>
-    </section>
+
+      <Carousel breakPoints={bpointovich}>
+        {products.length === 0 ? (
+          <Spinner />
+        ) : (
+          products.map((prod) => (
+            <div key={nanoid()}>
+              <Link to={`/products/${prod.id}`}>
+                <img src={prod.imagen} alt="product" />
+              </Link>
+
+              <h3>{prod.title}</h3>
+              <h3>${prod.price}</h3>
+            </div>
+          ))
+        )}
+      </Carousel>
+    </Seccion>
   );
 };
 
